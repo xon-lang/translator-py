b6f08ae18ba6e606c4513570a87650d738d1658a
nizami <nizami.dev@gmail.com>
2020-08-25 23:57:56 +0300

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    roots: ['<rootDir>/complex-tests', '<rootDir>/sandbox', '<rootDir>/src'],
+    roots: ['<rootDir>/sandbox', '<rootDir>/src'],
     testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
     transform: {
         '^.+\\.(ts|tsx)$': 'ts-jest',
